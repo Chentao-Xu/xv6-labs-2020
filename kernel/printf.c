10ddcb1fb74ad608e23e43bba64a6e1b877486a2
chentao <ct.xu@outlook.com>
2024-12-11 23:25:03 +0800

    定时中断功能，包括sigalarm和sigreturn系统调用，更新Makefile和相关文件以支持新功能

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,9 +121,22 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
+}
+
+// print backtrace
+void
+backtrace()
+{
+  printf("backtrace:\n");
+  uint64 fp = r_fp();
+  while (fp != PGROUNDUP(fp)) { // 未到达栈底
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16);
+  }
 }
 
 void
